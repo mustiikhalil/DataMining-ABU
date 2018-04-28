9576d2066ffd2185c212d51d43c65597f8fc9748
mustii <mustii@mmk.one>
2018-04-28 19:18:20 +0300

    Getting date and text to excel

--- main.py
+++ main.py
@@ -6,26 +6,25 @@
 """
 import Auth as AuthKeys
 import tweepy
-from database import Database
 import json
 import xlwt
 
 book = xlwt.Workbook()
 sh = book.add_sheet("1")
-
-data = Database()
-
 auth = tweepy.OAuthHandler(AuthKeys.consumer_key, AuthKeys.consumer_secret)
 auth.set_access_token(AuthKeys.access_token, AuthKeys.access_token_secret)
-
 api = tweepy.API(auth)
-
-
 user = api.get_user('Spotify')
 public_tweets = api.home_timeline()
-count = 0
+sh.write(0,0,"Tweet text")
+sh.write(0,1,"Date")
+count = 1
+
+
 for status in tweepy.Cursor(api.user_timeline, id='Spotify').items(3000):
+    date = str(status.created_at)
     sh.write(count,0,status.text)
+    sh.write(count,1,date)
     if count == 5:
         break 
     count += 1
